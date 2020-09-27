e61bbc064e89c4e56680dc93bf2542a366a7a2b2
Lubov_Frolova <l.frolova@i-teco.ru>
2020-09-27 19:04:53 +0300

    Ready

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(SquareCalculator.calculateSquare(0));
+        System.out.println(SquareCalculator.calculateSquare(2));
+        System.out.println(SquareCalculator.calculateSquare(100));
     }
 }
---
+++ src/main/java/SquareCalculator.java
@@ -0,0 +1,5 @@
+public class SquareCalculator {
+    public static float calculateSquare(int radius){
+        return (float) (radius * radius * 3.14);
+    }
+}
