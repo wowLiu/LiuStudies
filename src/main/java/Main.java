f0116983f08032264f9438f104e7f8c55c796fdf
Lubov_Frolova <l.frolova@i-teco.ru>
2020-09-27 19:55:39 +0300

    Ready

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(intoMeter(115));
+        System.out.println(intoMeter(800));
+        System.out.println(intoMeter(278));
+    }
+    public static int intoMeter(int sm){
+        return (int) Math.floor(Double.valueOf(sm / 100));
     }
 }
