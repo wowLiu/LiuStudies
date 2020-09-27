c8db7e47593ba507532468f7f3f59abc912aa49a
Lubov_Frolova <l.frolova@i-teco.ru>
2020-09-27 19:40:42 +0300

    Ready

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println(oddOrNot(14));
+        System.out.println(oddOrNot(19));
+    }
 
+    public static boolean oddOrNot(int n) {
+        return  n % 2 != 0;
+        // остаток при делении на два !не равен 0 (то, что после запятой). т.е. нечетное
     }
 }
