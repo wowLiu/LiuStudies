2d453c3ba2d5e2d74e25276d44754623d954692f
Lubov_Frolova <l.frolova@i-teco.ru>
2020-10-07 18:42:32 +0300

    lesson 1 task 6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(method1(false, false, false, false));
+        System.out.println(method1(true, true, true, true));
+        System.out.println(method1(false, true, false, true));
+        System.out.println(method1(true, false, true, false));
+        System.out.println(method1(true, true, false, false));
     }
+
+    private static boolean method1(boolean a, boolean b, boolean c, boolean d) {
+        return (a && b) && !(c || d);
+    }
+
 }
+//
+//true || true = true
+//true || false = true
+//false || true = true
+//false || false = false
+
+//true && true = true
+//true && false = false
+//false && true = false
+//false && false = false
